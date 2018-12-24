05713d6924d0f4fbc4f306d19aa47b04319e3d35
Mark Jordan <mjordan@sfu.ca>
2018-12-24 15:31:06 -0800

    Work on including media.

--- src/Command/CreateBagCommand.php
+++ src/Command/CreateBagCommand.php
@@ -44,6 +44,15 @@ class CreateBagCommand extends ContainerAwareCommand
         $this->settings = Yaml::parseFile($settings_path);
         $this->settings['drupal_base_url'] .= '/node/';
 
+        $this->settings['http_timeout'] = (!isset($this->settings['http_timeout'])) ?
+            60 : $this->settings['http_timeout'];
+        $this->settings['verify_ca'] = (!isset($this->settings['verify_ca'])) ?
+            true : $this->settings['verify_ca'];
+        $this->settings['include_json_data'] = (!isset($this->settings['include_json_data'])) ?
+            true : $this->settings['include_json_data'];
+        $this->settings['include_jsonld_data'] = (!isset($this->settings['include_jsonld_data'])) ?
+            true : $this->settings['include_jsonld_data'];                        
+
         if (!file_exists($this->settings['output_dir'])) {
             mkdir($this->settings['output_dir']);
         }
@@ -85,7 +94,7 @@ class CreateBagCommand extends ContainerAwareCommand
             mkdir($bag_temp_dir);
         }
 
-        $media_file_paths = $this->fetch_media($nid, $bag_temp_dir);
+        $data_file_paths = $this->fetch_media($nid, $bag_temp_dir);
 
         // Assemble data files. Fow now we only have one.
         $data_files = array();
@@ -103,8 +112,8 @@ class CreateBagCommand extends ContainerAwareCommand
         }
         $bag = new \BagIt($bag_dir, true, true, true, $bag_info);
         $bag->addFile($turtle_file_path, basename($turtle_file_path));
-        foreach ($media_file_paths as $media_file_path) {
-            $bag->addFile($media_file_path, basename($media_file_path));
+        foreach ($data_file_paths as $data_file_path) {
+            $bag->addFile($data_file_path, basename($data_file_path));
         }
 
         foreach ($this->settings['bag-info'] as $key => $value) {
@@ -142,17 +151,39 @@ class CreateBagCommand extends ContainerAwareCommand
         $media_url = $this->settings['drupal_base_url'] . $nid . '/media';
         $media_response = $media_client->request('GET', $media_url, [
             'http_errors' => false,
-            'auth' => $this->settings['drupal_media_auth'], 
+            'auth' => $this->settings['drupal_media_auth'],
             'query' => ['_format' => 'json']
         ]);
         $media_status_code = $media_response->getStatusCode();
         $media_list = (string) $media_response->getBody();
+        $json_data = $media_list;
         $media_list = json_decode($media_list, true);
+
+        $data_file_paths = array();
+        if ($this->settings['include_json_data']) {
+            // JSON data about media for this node.
+            $json_data_file_path = $bag_temp_dir . DIRECTORY_SEPARATOR . 'media.json';
+            $data_file_paths[] = $json_data_file_path;
+            file_put_contents($json_data_file_path, $json_data);
+        }
+        if ($this->settings['include_jsonld_data']) {
+            // JSON-LS data about media for this node.
+            $jsonld_client = new \GuzzleHttp\Client();
+            $jsonld_url = $this->settings['drupal_base_url'] . $nid . '/media';
+            $jsonld_response = $media_client->request('GET', $media_url, [
+                'http_errors' => false,
+                'auth' => $this->settings['drupal_media_auth'],
+                'query' => ['_format' => 'jsonld']
+            ]);
+            $jsonld_data = (string) $jsonld_response->getBody();
+            $jsonld_data_file_path = $bag_temp_dir . DIRECTORY_SEPARATOR . 'media.jsonld';
+            $data_file_paths[] = $jsonld_data_file_path;            
+            file_put_contents($jsonld_data_file_path, $jsonld_data);
+        }
 
         // Loop through all the media and pick the ones that are tagged with terms in $taxonomy_terms_to_check.
         foreach ($media_list as $media) {
             if (count($media['field_media_use'])) {
-                $media_file_paths = array();
                 foreach ($media['field_media_use'] as $term) {
                     if (in_array($term['url'], $this->settings['drupal_media_tags'])) {
                         if (isset($media['field_media_image'])) {
@@ -165,18 +196,18 @@ class CreateBagCommand extends ContainerAwareCommand
                         // Fetch file and save it to $bag_temp_dir with its original filename.
                         $file_client = new \GuzzleHttp\Client();
                         $file_response = $file_client->get($file_url, ['stream' => true,
-                            //'timeout' => $this->settings['http_timeout'],
-                            //'connect_timeout' => $this->settings['http_timeout'],
-                            //'verify' => $this->settings['verifyCA']
+                            'timeout' => $this->settings['http_timeout'],
+                            'connect_timeout' => $this->settings['http_timeout'],
+                            'verify' => $this->settings['verify_ca']
                         ]);
                         $file_body = $file_response->getBody();
                         while (!$file_body->eof()) {
                             file_put_contents($temp_file_path, $file_body->read(2048), FILE_APPEND);
                         }
-                        $media_file_paths[] = $temp_file_path;
+                        $data_file_paths[] = $temp_file_path;
                     }
                 }
-                return $media_file_paths;
+                return $data_file_paths;
             }
         }
     }
