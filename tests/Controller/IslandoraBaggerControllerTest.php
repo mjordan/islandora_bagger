4c59f0dc2007c03e88d4f92dab199f26ab1b61e6
Jared Whiklo <jwhiklo@gmail.com>
2022-05-18 15:25:07 -0500

    add getQueue tests

--- tests/Controller/IslandoraBaggerControllerTest.php
+++ tests/Controller/IslandoraBaggerControllerTest.php
@@ -60,6 +60,25 @@ class IslandoraBaggerControllerTest extends WebTestCase
     }
 
     /**
+     * Test add to queue to create bag.
+     * @covers \App\Controller\IslandoraBaggerController::create
+     */
+    public function testCreateNormal(): void
+    {
+        $app_dir = dirname(__DIR__, 2) . '/var/islandora_bagger.7.yml';
+        $this->client->request('POST', '/api/createbag', [], [], [
+          'HTTP_ISLANDORA_NODE_ID' => '7'
+        ]);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame("Content-type", "application/json");
+        $response = $this->client->getResponse();
+        $this->assertEquals(
+          '["Entry for node 7 using configuration at ' . addcslashes($app_dir, '/') . ' added to queue."]',
+          $response->getContent()
+        );
+    }
+
+    /**
      * When the location log path doesn't exist you get an empty response.
      * @covers \App\Controller\IslandoraBaggerController::getLocation
      */
@@ -116,6 +135,7 @@ class IslandoraBaggerControllerTest extends WebTestCase
           ]
         );
         $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame("Content-type", "application/json");
         $response = $this->client->getResponse();
         $json = json_decode($response->getContent(), true);
         $this->assertArrayHasKey("nid", $json);
@@ -125,5 +145,83 @@ class IslandoraBaggerControllerTest extends WebTestCase
         $this->assertArrayHasKey("created", $json);
         $this->assertEquals($date, $json["created"]);
         unlink($location_file);
+    }
+
+    /**
+     * Test getting an empty queue.
+     * @covers \App\Controller\IslandoraBaggerController::getQueue
+     */
+    public function testGetQueueEmpty(): void
+    {
+        $this->client->request('GET', '/api/queue');
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame("Content-type", "application/json");
+        $this->assertSame(
+          '{"message":"Queue file not found at ' . addcslashes($this->queue_path, '/') . '"}',
+          $this->client->getResponse()->getContent()
+        );
+    }
+
+    /**
+     * Test getting a queue with one record.
+     * @covers \App\Controller\IslandoraBaggerController::getQueue
+     */
+    public function testGetQueueWithRecord(): void
+    {
+        $yaml_path = dirname(__DIR__, 2) . '/var/islandora_bagger.99.yml';
+        // Populate the queue.
+        $this->client->request('POST', '/api/createbag', [], [], [
+          'HTTP_ISLANDORA_NODE_ID' => '99'
+        ]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $this->client->request('GET', '/api/queue');
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame("Content-type", "application/json");
+        // Use startsWith to avoid the hassle of matching the datetime exactly.
+        $this->assertStringStartsWith(
+          '["99\t' . addcslashes($yaml_path, '/') . '\t',
+          $this->client->getResponse()->getContent()
+        );
+    }
+
+    /**
+     * Test getting a queue with multiple records.
+     * @covers \App\Controller\IslandoraBaggerController::getQueue
+     */
+    public function testGetQueueWithMultipleRecords(): void
+    {
+        $yaml_path_prefix = dirname(__DIR__, 2) . '/var/islandora_bagger.';
+        $ids = [];
+        // Generate some random numbers
+        for ($x=0; $x<3; $x+=1) {
+            do {
+                // nothing
+            } while (in_array($id = rand(1, 100), $ids));
+            $ids[] = $id;
+        }
+
+        $expected_lines = [];
+        // Populate the queue.
+        foreach ($ids as $id) {
+            $this->client->request('POST', '/api/createbag', [], [], [
+              'HTTP_ISLANDORA_NODE_ID' => $id,
+            ]);
+            $this->assertResponseStatusCodeSame(200);
+            $expected_lines[] = $id . "\t" . $yaml_path_prefix . $id . ".yml\t";
+        }
+        $this->client->request('GET', '/api/queue');
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame("Content-type", "application/json");
+        // Use startsWith to avoid the hassle of matching the datetime exactly.
+        $json = json_decode($this->client->getResponse()->getContent());
+        $this->assertIsArray($json);
+        $this->assertCount(3, $json);
+        for ($x=0; $x<3; $x+=1) {
+            $this->assertStringStartsWith(
+              $expected_lines[$x],
+              $json[$x]
+            );
+        }
     }
 }
